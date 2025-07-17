37d107dec2f45c29837a1016f11a4568ea86c75d
Rroopesh Hari <rroopesh.hari@okstate.edu>
2025-07-17 03:43:23 -0500

    Initial commit of complete project structure

---
+++ services/icd_prediction/icd_classifier.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+cpt_df = pd.read_csv("C:\\Users\\dell\\Documents\\Python\\AI-medical-project\\data\\D_CPT.csv")
---
+++ services/icd_prediction/semantic_search.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+icd_df = pd.read_csv("C:\\Users\\dell\\Documents\\Python\\AI-medical-project\\data\\clean_icd10.csv")
